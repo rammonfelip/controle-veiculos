a856e11ee78c82fffa901e2f7d2568dfc171daa2
Felipe Ramon <rammonfelip@gmail.com>
2022-12-22 18:54:54 -0300

    Consultando e listando as manutenções no dashboard.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,16 +5,20 @@ namespace App\Http\Controllers;
 use App\Repositories\VeiculoRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 class DashboardController extends Controller
 {
     public function dashboard(VeiculoRepository $veiculoRepository)
     {
+        $userId = Auth::id();
+
         $veiculos = $veiculoRepository->findBy([
-            'user_id'   =>  Auth::id()
+            'user_id'   =>  $userId
         ])->count();
 
-        $manutencoes = [];
+        $request = Request::create(env('APP_URL') . sprintf('/api/v1/%s/manutencoes', $userId), 'GET');
+        $manutencoes = json_decode(Route::dispatch($request)->getContent())->data;
 
         return view('dashboard', compact('veiculos', 'manutencoes'));
     }
